26cf93831603494897337cbd6868c1bb7f656c8e
Marcus Longmuir <marcus@fusioncloud.com>
2013-03-02 02:59:52 +0000

    Removed auto-generated javadoc.

--- RawJSON.java
+++ RawJSON.java
@@ -1,67 +1,30 @@
-/*
- * 
- */
 package org.json;
+
 /**
- * The Class RawJSON.
- * @author marcuslongmuir
- * @version $Revision: 1.0 $
+ * Used to hold a known valid JSON string in a JSON structure
+ * without the overhead of parsing it first. 
+ * @author Marcus Longmuir
+ * @version 2013-03-01
  */
 public class RawJSON {
 
-	/** The raw json. */
 	String rawJSON = null;
-	public Object data = null;
 
-	/**
-	 * Instantiates a new raw json.
-	 *
-	 * @param string the string
-	 */
 	public RawJSON(String string){
 		this.rawJSON = string;
 	}
 
-	/**
-	 * Constructor for RawJSON.
-	 * @param string1 String
-	 * @param object Object
-	 */
-	public RawJSON(String string1, Object object){
-		this.rawJSON = string1;
-		this.data = object;
-	}
-
-	/**
-	 * Sets the string.
-	 *
-	 * @param toSet the new string
-	 */
 	public void setString(String toSet){
 		this.rawJSON = toSet;
 	}
 
-	/**
-	 * Clone other raw json.
-	 *
-	 * @param other the other
-	 */
-	public void cloneOtherRawJSON(RawJSON other){
+	public void useContentFrom(RawJSON other){
 		this.rawJSON = other.rawJSON;
 	}
-
-	/**
-	 * Clone json object.
-	 *
-	 * @param obj the obj
-	 */
 	public void cloneJSONObject(JSONObject obj){
 		this.rawJSON = obj.toString();
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
 	public String toString(){
 		return this.rawJSON;
 	}
